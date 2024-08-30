766950f0b2467371d2f4d116b420974ead83567d
maha15625 <muthusubbu15625@gmail.com>
2024-08-30 00:34:54 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,30 @@
+function add(){
+    let a=number(document.getElementById("value1"),value);
+    let b=number(document.getElementById("value2"),value);
+    let c=a+b;
+    console.log("result:"),InnerHTML="sum of"+a+" and "+b+" is"+c;
+}
+function sub(){
+    let a=number(document.getElementById("value1"),value);
+    let b=number(document.getElementById("value2"),value);
+    let c=a-b;
+    console.log("result:"),InnerHTML="sum of"+a+" and "+b+" is"+c;
+}
+function mul(){
+    let a=number(document.getElementById("value1"),value);
+    let b=number(document.getElementById("value2"),value);
+    let c=a*b;
+    console.log("result:"),InnerHTML="sum of"+a+" and "+b+" is"+c;
+}
+function div(){
+    let a=number(document.getElementById("value1"),value);
+    let b=number(document.getElementById("value2"),value);
+    let c=a/b;
+    console.log("result:"),InnerHTML="sum of"+a+" and "+b+" is"+c;
+}
+function exp(){
+    let a=number(document.getElementById("value1"),value);
+    let b=number(document.getElementById("value2"),value);
+    let c=a%b;
+    console.log("result:"),InnerHTML="sum of"+a+" and "+b+" is"+c;
+}
---
+++ pro.js
@@ -0,0 +1,69 @@
+const p1 = 3.14159;
+
+console.log(p1);
+
+ let age =25;
+ const name ="john";
+ var isStudent= true;
+
+ let person ={
+    firstname: "john",
+    lastname: "doe",
+    age: 25,
+    isstudent: true
+ };
+  
+ console.log(name,age,isStudent);
+ console.log(person);
+ let a=30;
+ let r=p1*a**2;
+ console.log("area of circle=",r);
+ 
+ let b=5;
+ let h=2;
+ let an=0.5*b*h;
+ console.log("area of triangle=",h);
+
+ let s=15;
+ let m=s*s;
+ console.log("area of rectangle=",m);
+
+ let ms=4;
+ let mm=ms*ms*ms;
+ console.log("area of square=",mm);
+
+ let ss=15;
+ let aa=p1*ss*2/2;
+ console.log("area of semi circle=",aa);
+
+ let cc=35;
+ let dd=cc*2*p1*(r+h);
+ console.log("area of cylinder=",cc);
+
+ let vv=25;
+ let kk=vv*3;
+ console.log("area of cube=",kk);
+
+ let NAME="Mahlakshmi";
+ let sub1=80;
+ let sub2=90;
+ let sub3=70;
+ let sub4=90;
+ let sub5=90;
+
+ let marks=sub1+sub2+sub3+sub4+sub5;
+ let average=(sub1+sub2+sub3+sub4+sub5)/5;
+ console.log("STUDENT NAME=",name);
+ console.log("TAMIL=",sub1);
+ console.log("ENGLISH=",sub2);
+ console.log("MATHS=",sub3);
+ console.log("SCIENCE=",sub4);
+ console.log("SOCIAL=",sub5);
+ console.log("TOTAL=",marks);
+ console.log("AVERAGE=",average);
+
+
+
+
+
+
